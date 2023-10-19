bf716cf250ea6deb13083121cc3bd5677ad80e42
Hayeon LEE <fhdodl@naver.com>
2023-10-19 21:49:41 +0900

    feat : 회원가입 기능 구현 #2

--- fes7-api/src/App.js
+++ fes7-api/src/App.js
@@ -1,9 +1,54 @@
+import { useState } from "react";
+import JoinPage from "./components/JoinPage";
 import LoginPage from "./components/LoginPage";
+import axios from "axios"; // Import Axios
+
 function App() {
+  const [page, setPage] = useState(true);
+  const [info, setInfo] = useState("");
+
+  const handlePage = () => {
+    setPage((prevPage) => {
+      return !prevPage;
+    });
+  };
+
+  // axios로 변환
+  const getMyinfo = async () => {
+    const token = localStorage.getItem("token");
+    console.log(token);
+
+    try {
+      const response = await axios.get(
+        "https://api.mandarin.weniv.co.kr/user/myinfo",
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      const json = response.data;
+      console.log(json);
+      setInfo(JSON.stringify(json));
+    } catch (error) {
+      console.error("에러 발생", error);
+    }
+  };
+
   return (
-    <>
-      <LoginPage/>
-    </>
+    <div>
+      <button type="button" onClick={getMyinfo}>
+        내 정보 불러오기
+      </button>
+      {info}
+      <JoinPage handlePage={handlePage} />
+      {page ? (
+        <LoginPage handlePage={handlePage} />
+      ) : (
+        <JoinPage handlePage={handlePage} />
+      )}
+    </div>
   );
 }
+
 export default App;
--- fes7-api/src/components/JoinPage.jsx
+++ fes7-api/src/components/JoinPage.jsx
@@ -1,93 +1,112 @@
-import { useState } from "react"
+import { useState } from "react";
+import axios from "axios";
 
-const JoinPage = ()=>{
-//     {
-// 		"user": {
-// 				"username": String*,
-// 				"email": String*,
-// 				"password": String*,
-// 				"accountname": String*,
-// 				"intro": String,
-// 				"image": String // 예시) https://api.mandarin.weniv.co.kr/1641906557953.png
-// 		}
-// } 
-    const [username, setUsername] = useState("");
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [accountname, setAccountname] = useState("");
+const JoinPage = ({ handlePage }) => {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [accountname, setAccountname] = useState("");
 
-    // 요청코드
-    const join =  (joinData)=>{
-        const reqUrl = 'https://api.mandarin.weniv.co.kr';
-        fetch(reqUrl, {
-          method : 'POST',
-          headers : {
-            "Content-type":"application/json"
-          },
-          body:JSON.stringify(joinData)
-        })
+  const join = async (joinData) => {
+    const reqUrl = "https://api.mandarin.weniv.co.kr/user";
+    try {
+      const res = await axios.post(reqUrl, joinData, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      console.log(res.data);
+    } catch (error) {
+      console.error("Error while joining:", error);
     }
-    
-    const inputUsername = (e)=>{
-        setUsername(e.target.value);
-    }
-    const inputEmail = (e)=>{
-        setEmail(e.target.value);
-    }
-    const inputPassword = (e)=>{
-        setPassword(e.target.value);
-    }
-    const inputAccountname = (e)=>{
-        setAccountname(e.target.value);
-    }
+  };
 
-    const submitJoin = ()=>{
-        const joinData = {
-            username:username,
-            email:email,
-            password:password,
-            accountname:accountname
-        }
-        join(joinData);
-    }
+  const inputUsername = (e) => {
+    setUsername(e.target.value);
+  };
+  const inputEmail = (e) => {
+    setEmail(e.target.value);
+  };
+  const inputPassword = (e) => {
+    setPassword(e.target.value);
+  };
+  const inputAccountname = (e) => {
+    setAccountname(e.target.value);
+  };
 
-    return(
-        <>
-        <section >
-            <h2 >이메일로 회원가입</h2>
-            <div >
-                <label htmlFor="emailInput">이메일</label>
-                <input value={email} onChange={inputEmail} type="email" id="emailInput" name="email" placeholder="이메일 주소를 알려주세요."/>
-            </div>
-            <div>
-                <label htmlFor="passwordInput">비밀번호</label>
-                <input value={password} onChange={inputPassword} type="password" name="password" id="passwordInput" placeholder="비밀번호를 설정해 주세요."/>
-            </div>
-            <button type="button" >다음</button>
-        </section>
-        <section>
-            <h2 >프로필 설정</h2>
-            <p>나중에 언제든지 변경할 수 있습니다.</p>
-            <label htmlFor="profileImg">
-                <img src="https://api.mandarin.weniv.co.kr/Ellipse.png" alt="" id="imagePre"/>
-            </label>
-            <input type="file" id="profileImg" name="image" accept="image/*" />
-            <div >
-                <label htmlFor="userNameInput">사용자 이름</label>
-                <input value={username} onChange={inputUsername} type="text" id="userNameInput" name="username" placeholder="2~10자 이내여야 합니다."/>
-            </div>
-            <div >
-                <label htmlFor="userIdInput">계정 ID</label>
-                <input value={accountname} onChange={inputAccountname} type="text" id="userIdInput" name="accountname" placeholder="영문, 숫자, 특수문자(,), (_)만 사용 가능합니다."/>
-            </div>
-            <div>
-                <label htmlFor="userIntroInput">소개</label>
-                <input type="text" id="userIntroInput" name="intro" placeholder="자신과 판매할 상품에 대해 소개해 주세요."/>
-            </div>
-            <button type="button" onClick={submitJoin} >감귤마켓 시작하기</button>
-        </section>
-        </>
-    )
-}
+  const submitJoin = () => {
+    const joinData = {
+      user: {
+        username: username,
+        email: email,
+        password: password,
+        accountname: accountname,
+      },
+    };
+    join(joinData);
+  };
 
-export default JoinPage
+  return (
+    <>
+      <button type="button" onClick={handlePage}>
+        로그인페이지로 돌아가기
+      </button>
+      <section>
+        <h2>이메일로 회원가입</h2>
+        <div>
+          <label htmlFor="emailInput">이메일</label>
+          <input
+            value={email}
+            onChange={inputEmail}
+            type="email"
+            id="emailInput"
+            name="email"
+            placeholder="이메일 주소를 알려주세요."
+          />
+        </div>
+        <div>
+          <label htmlFor="passwordInput">비밀번호</label>
+          <input
+            value={password}
+            onChange={inputPassword}
+            type="password"
+            name="password"
+            id="passwordInput"
+            placeholder="비밀번호를 설정해 주세요."
+          />
+        </div>
+        <button type="button">다음</button>
+      </section>
+      <section>
+        <h2>프로필 설정</h2>
+        <div>
+          <label htmlFor="userNameInput">사용자 이름</label>
+          <input
+            value={username}
+            onChange={inputUsername}
+            type="text"
+            id="userNameInput"
+            name="username"
+            placeholder="2~10자 이내여야 합니다."
+          />
+        </div>
+        <div>
+          <label htmlFor="userIdInput">계정 ID</label>
+          <input
+            value={accountname}
+            onChange={inputAccountname}
+            type="text"
+            id="userIdInput"
+            name="accountname"
+            placeholder="영문, 숫자, 특수문자(,), (_)만 사용 가능합니다."
+          />
+        </div>
+        <button type="button" onClick={submitJoin}>
+          감귤마켓 시작하기
+        </button>
+      </section>
+    </>
+  );
+};
+
+export default JoinPage;
