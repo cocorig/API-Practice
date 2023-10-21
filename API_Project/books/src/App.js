2890ddbe464613c3545e29378ecdf4bbd9c763d5
Hayeon LEE <fhdodl@naver.com>
2023-10-21 23:55:05 +0900

    feat : 컴포넌트 추가

--- API_Project/books/src/App.js
+++ API_Project/books/src/App.js
@@ -1,40 +1,15 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-
+import Bestseller from "./Bestseller";
+import ItemNewAll from "./ItemNewAll";
+import DetailBookInfo from "./DetailBookInfo";
 function ProductList() {
-  const [productList, setProductList] = useState([]);
-
-  useEffect(() => {
-    axios
-      .get("http://localhost:3000/productList") // Replace with your API endpoint
-      .then((response) => {
-        const item = response.data.item;
-        setProductList(item);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  }, []);
-
   return (
-    <div>
-      <h1>베스트 셀러</h1>
-      <ul>
-        {productList.map((product) => (
-          <li key={product.itemId}>
-            <div>
-              <img src={product.cover} alt={product.title} />
-            </div>
-            <div>
-              <h2>제목:{product.title}</h2>
-              <p>저자:{product.author}</p>
-              <p>가격: {product.priceSales} KRW</p>
-              <p>{product.publisher}</p>
-            </div>
-          </li>
-        ))}
-      </ul>
-    </div>
+    <>
+      <Bestseller />
+      <ItemNewAll />
+      {/* <DetailBookInfo /> */}
+    </>
   );
 }
 
