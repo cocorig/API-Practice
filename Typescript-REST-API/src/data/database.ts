0c52845110b1bbc383b031acb6e73b02b521b25f
Hayeon LEE <fhdodl@naver.com>
2024-03-05 23:41:14 +0900

    feat : 데이터베이스 연결

---
+++ Typescript-REST-API/src/data/database.ts
@@ -0,0 +1,25 @@
+// db 연결
+import { createConnection } from "typeorm";
+import dotenv from "dotenv";
+dotenv.config();
+const { DB_HOST, DB_PORT, DB_USERNAME, DB_PASSWORD, DB_DATABASE } = process.env;
+export const db = createConnection({
+  type: "mysql", // 사용할 데이터베이스 종류
+  host: DB_HOST,
+  port: Number(DB_PORT),
+  username: DB_USERNAME, // 데이터베이스 사용자 이름
+  password: DB_PASSWORD, // 데이터베이스 비밀번호
+  database: DB_DATABASE, // 사용할 데이터베이스 이름
+  entities: [], // TypeORM이 사용할 엔티티 목록
+  synchronize: true, // 엔티티 스키마와 데이터베이스 스키마를 동기화할지 여부 (개발 환경에서는 true로 설정하는 것이 일반적)
+})
+  .then((connection) => {
+    console.log("Successful database connection!");
+  })
+  .catch((error) => {
+    console.error("The connection to database was failed with error:", error);
+  });
+// entities란 TypeORM에서 데이터베이스와 매핑할 엔티티(Entity)의 배열이다.
+// 엔티티는 데이터베이스 테이블을 나타내는 TypeScript 클래스이다.
+// typeORM은 데이터베이스의 테이블과 해당 엔티티를 자동으로 매핑하여 데이터베이스와 상호 작용할 수 있도록 도와주는 도구이다.
+//https://velog.io/@tilto0822/TypeORM-%EC%B2%98%EC%9D%8C-%EB%A7%8C%EB%82%98%EB%B3%B8-%ED%98%81%EC%8B%A0
--- Typescript-REST-API/src/index.ts
+++ Typescript-REST-API/src/index.ts
@@ -1,8 +1,15 @@
 import express from "express";
 import { user_route } from "./routes/user.route";
+import { db } from "./data/database";
+import dotenv from "dotenv";
+dotenv.config();
 const app = express();
+const PORT = process.env.PORT || 5000;
+
+db.then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server running at ${PORT}`);
+  });
+});
 
 app.use("/api/users", user_route);
-app.listen(3000, () => {
-  console.log("listening on port 3000");
-});
